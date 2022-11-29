e0ed23cf58c1953e2ab59b1295df196ea5e759e9
csdomokos <95182525+csdomokos@users.noreply.github.com>
2022-11-29 18:49:42 +0100

    Calculator with methods

---
+++ CalculatorMethod.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CalculatorMethod {
+    public static void main(String[] args) {
+
+        getInputFromUser();
+        int result = getCalculationResult(operator, number1, number2);
+        showResultToUser(result);
+    }
+
+    static String operator = "";
+    static int number1 = 0;
+    static int number2 = 0;
+
+
+    public static void getInputFromUser (){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me operator:");
+        operator = scanner.nextLine();
+        System.out.println("Give me number 1: ");
+        number1 = scanner.nextInt();
+        System.out.println("Give me number 2: ");
+        number2 = scanner.nextInt();
+    }
+
+    public static int getCalculationResult (String operator, int number1, int number2){
+        int result = 0;
+        if(operator.equals("+")){
+          result = number1+number2;
+        } else if (operator.equals("-")) {
+            result = number1-number2;
+        } else if (operator.equals("*")) {
+            result = number1*number2;
+        } else if (operator.equals("/")) {
+            result = number1/number2;
+        }
+        return result;
+    }
+
+    public static void showResultToUser(int result){
+        System.out.println("Result: " + getCalculationResult(operator,number1,number2));
+    }
+
+}
